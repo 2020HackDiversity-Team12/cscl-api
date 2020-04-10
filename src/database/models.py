419e4d50e2ea1881cbed385e75dc90985f6b28d4
Krishna Farvil <fkrishna.dev@gmail.com>
2020-04-10 02:01:40 -0400

    Add book model

--- src/database/models.py
+++ src/database/models.py
@@ -1,3 +1,17 @@
 ''' 
   MODELS MODULE
 '''
+from database import db
+
+
+class Book(db.Document):
+    isbn: db.StringField(required=True)
+    title: db.StringField(required=True)tr
+    author: db.StringField(required=True)
+    publisher: db.StringField(required=True)
+    image_url_s: db.URLField()
+    image_url_m: db.URLField()
+    image_url_l: db.URLField()
+    copies: db.IntField(default=0)
+    available: db.IntField(default=0)
+    publication_year: db.IntField(required=True)
