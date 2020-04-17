435a06aa6518b6b7e78d4eeb2c588d42e11aa4d4
Krishna Farvil <fkrishna.dev@gmail.com>
2020-04-16 20:59:48 -0400

    Comment dev config

--- src/config.py
+++ src/config.py
@@ -11,13 +11,13 @@ class Config:
 
 class ProdConfig(Config):
     DEBUG = False
-    MONGODB_DB = getenv('PROD_MONGODB_DB')
-    MONGODB_HOST = getenv('PROD_MONGODB_HOST')
-    MONGODB_PORT = int(getenv('PROD_MONGODB_PORT'))
+    MONGODB_DB = getenv('PROD_MONGODB_DB', 'cscl_db')
+    MONGODB_HOST = getenv('PROD_MONGODB_HOST', '3.20.216.39')
+    MONGODB_PORT = int(getenv('PROD_MONGODB_PORT', 27017))
 
 
-class DevConfig(Config):
-    DEBUG = True
-    MONGODB_DB = getenv('DEV_MONGODB_DB')
-    MONGODB_HOST = getenv('DEV_MONGODB_HOST')
-    MONGODB_PORT = int(getenv('DEV_MONGODB_PORT'))
+# class DevConfig(Config):
+#     DEBUG = True
+#     MONGODB_DB = getenv('DEV_MONGODB_DB')
+#     MONGODB_HOST = getenv('DEV_MONGODB_HOST')
+#     MONGODB_PORT = int(getenv('DEV_MONGODB_PORT'))
